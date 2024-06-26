0f347b9f40ad0ca705df78d34a86223c075cc820
osamaamr397 <47090033+osamaamr397@users.noreply.github.com>
2024-06-26 16:20:08 +0300

    fixing termination

--- src/test/java/com/amr/chat/ChatApplicationTests.java
+++ src/test/java/com/amr/chat/ChatApplicationTests.java
@@ -1,13 +1,19 @@
 package com.amr.chat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChatApplicationTests {
 
+	@Autowired
+	ChatApplication chatApplication;
+	
 	@Test
 	void contextLoads() {
+		assert(chatApplication != null);
+		
 	}
 
 }
